dc5191c9db597bafd90faa9996421d4e236b9e5a
Duy Nguyen <milonguyen95@outlook.com>
2021-02-06 01:36:58 +0700

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -3,8 +3,8 @@ const fs = require("fs")
 module.exports = {
     devServer: {
         https: {
-          key: process.env.VUE_APP_MODE !== ‘development’ ? '' : fs.readFileSync("./key.pem"),
-          cert: process.env.VUE_APP_MODE !== ‘development’ ? '' : fs.readFileSync("./cert.pem"),
+          key: process.env.NODE_ENV !== ‘development’ ? '' : fs.readFileSync("./key.pem"),
+          cert: process.env.NODE_ENV !== ‘development’ ? '' : fs.readFileSync("./cert.pem"),
         },
         public: "https://localhost:5555/"
     }
